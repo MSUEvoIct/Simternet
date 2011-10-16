634419ee270f6aa763d8f795da32e05067f6b60c
Kendall Koning <kkoning@gmail.com>
2011-10-16 00:47:02 -0400

    Altered shell script to faclitate running with data directory outside the source tree.

--- Launches/run.sh
+++ Launches/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-java -server -Xmx8192M -XX:ParallelGCThreads=24 -classpath ./bin:../ECJ/bin:../Mason/bin:./lib/log4j-1.2.16.jar ec.Evolve -file data/config/evolve-workstation.properties -p stat.gather-full=true
+if [ -z "$SIMTERNET_DIR" ]; then
+SIMTERNET_DIR=.
+fi
+
+java -server -Xmx8192M -XX:ParallelGCThreads=24 -classpath $SIMTERNET_DIR/bin:$SIMTERNET_DIR/../ECJ/bin:$SIMTERNET_DIR../Mason/bin:$SIMTERNET_DIR/lib/log4j-1.2.16.jar ec.Evolve -file data/config/evolve-workstation.properties -p stat.gather-full=true
