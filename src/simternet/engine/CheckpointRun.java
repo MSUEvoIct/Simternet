606b63cfa6bc626b7d1e5403d0a7615e7c6d7b24
Kendall Koning <kkoning@gmail.com>
2011-10-15 20:37:28 -0400

    Added a utility class to initiate a run of a Simternet checkpoint file, for a specified number of steps.  Extremely useful for tracing and debugging.

---
+++ src/simternet/engine/CheckpointRun.java
@@ -0,0 +1,27 @@
+package simternet.engine;
+
+import java.io.File;
+
+import sim.engine.SimState;
+
+public class CheckpointRun {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String checkpointFile = args[0];
+		Integer steps = Integer.parseInt(args[1]);
+
+		File checkpoint = new File(checkpointFile);
+
+		SimState s = SimState.readFromCheckpoint(checkpoint);
+
+		while (steps-- > 0) {
+			s.schedule.step(s);
+		}
+
+		System.exit(0);
+
+	}
+}
