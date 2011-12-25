ee5f711426e981964a55e0485f216d4998f141b1
kkoning <kkoning@gmail.com>
2011-12-24 21:16:04 -0400

    Significant additional comments for the CongestionAlgorithm interface

--- src/simternet/network/CongestionAlgorithm.java
+++ src/simternet/network/CongestionAlgorithm.java
@@ -7,11 +7,27 @@ import java.util.List;
  * limit the set of network flows passing through such a link so that they
  * conform with the simulated physical characteristics of that link.
  * 
+ * A Congestion algorithm works on the Simternet equivalent of a router input queue,
+ * though, rather than operating on the instant packets, it operates on a set of NetFlow
+ * objects that represent aggregated usage over an entire simulation time step.  This allows
+ * congestion algorithms to scale with the number of application / user pairs, but not 
+ * bandwidth, as processing 1:1 simulated packets would be remarkably inefficient for
+ * this type of simulation.
+ * 
  * @author kkoning
  * 
  */
 public interface CongestionAlgorithm {
 	
+	/** 
+	 * Accepts a list of NetFlows requesting resources, returns a (possibly altered) list 
+	 * of NetFlows which have passed through this link.
+	 * 
+	 * @param flows  A list of flows requesting resources on the specified backbone link.
+	 * @param bottleneck  The BackboneLink on which these flows are to be transmitted
+	 * @return A list of (possibly) altered NetFlow objects which now conform to the 
+	 *  characteristics of this congestion algorithm on the specified backbone link.
+	 */
 	public List<NetFlow> limit(List<NetFlow> flows, BackboneLink bottleneck);
 
 }
