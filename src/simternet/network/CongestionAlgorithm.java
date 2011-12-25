9bedcc749b53f047a43b6e298962efb5a8805763
kkoning <kkoning@gmail.com>
2011-12-24 21:01:18 -0400

    Added comment for CongestionAlgorithm interface

--- src/simternet/network/CongestionAlgorithm.java
+++ src/simternet/network/CongestionAlgorithm.java
@@ -2,8 +2,19 @@ package simternet.network;
 
 import java.util.List;
 
+/**
+ * Congestion algorithms are associated with BackboneLink; their role is to
+ * limit the set of network flows passing through such a link so that they
+ * conform with the simulated physical characteristics of that link.
+ * 
+ * @author kkoning
+ * 
+ */
 public interface CongestionAlgorithm {
-	public BackboneLink getLink();
+	
 
+	public BackboneLink getLink();
+	
 	public List<NetFlow> limit(List<NetFlow> flows, BackboneLink bottleneck);
+
 }
