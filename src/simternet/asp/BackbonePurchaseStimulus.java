0e5e042b7b00eaf28b9869eebeb3dbcbc161f049
kkoning <kkoning@gmail.com>
2013-07-06 14:32:48 -0400

    Added variables for customer information (aspCustomers, nspCustomers, and intersectionCustomers) and brief javadoc comments.

--- src/simternet/asp/BackbonePurchaseStimulus.java
+++ src/simternet/asp/BackbonePurchaseStimulus.java
@@ -1,6 +1,19 @@
 package simternet.asp;
 
 public class BackbonePurchaseStimulus {
+	/** The NSP we're purchasing from */
 	public int nspID;
+	
+	/** Price per unit of bandwidth */
 	public double price;
+
+	/** Total # of ASP Customers */
+	public double aspCustomers;
+	
+	/** Total # subscribed to NSP's edge networks */
+	public double nspCustomers;
+	
+	/** Total # of customers intersection */
+	public double intersectionCustomers;
+	
 }
